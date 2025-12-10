3e89d05734a277d0404fb0c5be8fecd241f3c644
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2025-12-10 21:02:35 +0200

    + preprocess in main screen

--- lib/ui/upload_pic.dart
+++ lib/ui/upload_pic.dart
@@ -1,15 +1,11 @@
 import 'dart:io';
-
 import 'package:camera/camera.dart';
 import 'package:clothes_image_classification/helper/image_preprocessing.dart';
 import 'package:clothes_image_classification/utils/app_colors.dart';
 import 'package:clothes_image_classification/utils/app_images.dart';
 import 'package:clothes_image_classification/utils/app_styles.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as img;
-
 import '../utils/widgets/custom_elevated_button.dart';
 import 'camera_preview_screen.dart';
 
@@ -190,23 +186,23 @@ class _UploadPicState extends State<UploadPic> {
   Future<void> submit() async{
     // ChosenPicture.image = await convertFileToImage(imageFile: imageFile ?? File(_imagePath!.path));
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: CircularProgressIndicator(color: AppColors.primary,))
+      SnackBar(content: Text('Processing...',style: AppStyles.white20bold_poppins,),shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(10)),backgroundColor: AppColors.primary,persist: true,)
     );
     await ImagePreprocessing.imagePreprocessing(imageFile: imageFile ?? File(_imagePath!.path));
-      SnackBar(content:Text("done?"));
+    ScaffoldMessenger.of(context).clearSnackBars();
   }
 
-  Future<img.Image> convertFileToImage({required File imageFile}) async {
-    // Read file directly as bytes
-    List<int> imageBytes = await imageFile.readAsBytes();
-    // Decode
-    img.Image? image = img.decodeImage(Uint8List.fromList(imageBytes));
-
-    if (image == null) {
-      throw Exception('Failed to decode image');
-    }
-    return image;
-  }
+  // Future<img.Image> convertFileToImage({required File imageFile}) async {
+  //   // Read file directly as bytes
+  //   List<int> imageBytes = await imageFile.readAsBytes();
+  //   // Decode
+  //   img.Image? image = img.decodeImage(Uint8List.fromList(imageBytes));
+  //
+  //   if (image == null) {
+  //     throw Exception('Failed to decode image');
+  //   }
+  //   return image;
+  // }
 
 
 }
