3e20cc520e4ca7d3d10fe605562bee8829bd06b4
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2025-12-10 22:55:12 +0200

    fix: change invertedImage type from Uint8List to img.Image

--- lib/model/chosen_picture.dart
+++ lib/model/chosen_picture.dart
@@ -3,6 +3,6 @@ import 'package:flutter/foundation.dart';
 class ChosenPicture {
   static img.Image? image;
   static img.Image? grayScaleImage;
-  static Uint8List? invertedImage;
+  static img.Image? invertedImage;
   static List<List<double>>? finalResult;
 }
