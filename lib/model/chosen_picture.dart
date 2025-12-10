7c69ea4a7e81163a5434c7ad57a87e27266a971a
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2025-12-10 21:02:56 +0200

    + fix/remove unused import

--- lib/model/chosen_picture.dart
+++ lib/model/chosen_picture.dart
@@ -1,8 +1,7 @@
 import 'package:image/image.dart' as img;
-import 'package:flutter/foundation.dart';
 class ChosenPicture {
   static img.Image? image;
   static img.Image? grayScaleImage;
-  static Uint8List? invertedImage;
+  static img.Image? invertedImage;
   static List<List<double>>? finalResult;
 }
