a3070d98ae997b37f0a6f4d2e65fa9962eb70caa
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2025-12-10 17:16:26 +0200

    + add app styles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,32 @@
+import 'package:clothes_image_classification/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyles {
+  // primary
+  static final TextStyle primary20bold_poppins= GoogleFonts.poppins(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: AppColors.primary
+  );
+  static final TextStyle primary24bold_tiroTamil= GoogleFonts.tiroTamil(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: AppColors.primary
+  );
+  // black
+  static final TextStyle black24bold_poppins= GoogleFonts.poppins(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.black
+  );
+  // white
+  static final TextStyle white20bold_poppins= GoogleFonts.poppins(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: AppColors.white
+  );
+
+
+
+}
